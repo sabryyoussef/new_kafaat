6d31752b284bbf1235f174551dcf83b35d311dd3
sabry youssef <vendorah2@gmail.com>
2025-11-24 15:41:35 +0200

    Add empty placeholders for all moved document_request_portal files to satisfy database cache

---
+++ custom_addons/grants_training_suite_v19/models/document_request_portal.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# This model has been moved to student_documents_portal module
+# Keeping empty placeholder to satisfy cached imports
+# TODO: This file can be safely deleted after upgrading to student_documents_portal module
+
